81e195010b0d21abead25efd182a47c8f147e374
romflorod <romanflodriguez13@gmail.com>
2023-08-21 23:18:25 +0200

    registro

--- game/game/urls.py
+++ game/game/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include  
 from player import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('mostrar_razas/', views.mostrar_razas),
-
+    path('home/', views.home, name='home'),  # Agrega esta URL para la vista de la página de inicio
+    path('register/', views.register, name='register'),  # Agrega esta URL para la vista de registro
 ]
--- game/player/views.py
+++ game/player/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from .models import Draconido, Elfo, Enano, Gnomo, Humano, Mediano, Semielfo, Semiorco, Tiefling
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
+def home(request):
+    # Tu lógica de vista aquí
+    return render(request, 'home.html')  
 def mostrar_razas(request):
     razas = {
         'Draconido': Draconido.objects.all(),
@@ -14,3 +20,16 @@ def mostrar_razas(request):
         'Tiefling': Tiefling.objects.all(),
     }
     return render(request, 'mostrar_razas.html', {'razas': razas})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Inicia sesión automáticamente al usuario después de registrarse
+            login(request, user)
+            return redirect('home')  # Cambia 'home' por la URL a la que quieras redirigir al usuario después de registrarse
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
